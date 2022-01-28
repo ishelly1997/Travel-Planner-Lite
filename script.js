5286cea62e10b3c9936b3f8cafb79c6d7cd0b1ee
ishelly1997 <ishelly1997@gmail.com>
2022-01-28 13:16:03 -0500

    exchange rate api

---
+++ script.js
@@ -0,0 +1,10 @@
+var requestURL = 'https://api.exchangerate.host/latest';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var response = request.response;
+  console.log(response);
+}
